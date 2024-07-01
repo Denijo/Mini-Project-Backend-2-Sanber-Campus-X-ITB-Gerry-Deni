11abcd2524c05af8cb458ea980df28bb4250a963
GarryAma <amasiagarry23@gmail.com>
2024-07-01 14:30:53 +1000

    adding few changes and updated nodemon installed

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,14 +1,19 @@
-const express = require('express')
-const { authentication } = require('../middleware/basicAuth')
-const { getAllReview, getReviewById, createReview, updateReview, deleteReview } = require('../controllers/reviewController')
-const { reviewCheckById } = require('../middleware/reviewCheck')
-const router = express.Router()
+const express = require("express");
+const { authentication } = require("../middleware/basicAuth");
+const {
+  getAllReview,
+  getReviewById,
+  createReview,
+  updateReview,
+  deleteReview,
+} = require("../controllers/reviewController");
+const { reviewCheckById } = require("../middleware/reviewCheck");
+const router = express.Router();
 
-router.get('/review-bike', getAllReview)
-router.get('/review-bike/:id', reviewCheckById, getReviewById)
-router.post('/review-bike', createReview)
-router.put('/review-bike/:id', reviewCheckById, updateReview)
-router.delete('/review-bike/:id', reviewCheckById, deleteReview)
+router.get("/review-bike", getAllReview);
+router.get("/review-bike/:id", reviewCheckById, getReviewById);
+router.post("/review-bike", createReview);
+router.put("/review-bike/:id", reviewCheckById, updateReview);
+router.delete("/review-bike/:id", reviewCheckById, deleteReview);
 
-module.exports = router
-
+module.exports = router;
